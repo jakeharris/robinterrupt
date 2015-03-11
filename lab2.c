18cc9b6e489531ac6e6aa6b62deef9a8e2e800c3
Jake Harris <milieu@javakat.io>
2015-03-11 12:49:06 -0500

    Queue events

--- lab2.c
+++ lab2.c
@@ -21,7 +21,7 @@
 /*****************************************************************************\
 *                             Global definitions                              *
 \*****************************************************************************/
-
+#define MAX_NUMBER_EVENTS 32
 
 
 
@@ -29,15 +29,32 @@
 /*****************************************************************************\
 *                            Global data structures                           *
 \*****************************************************************************/
+typedef struct EventQueue {
+  int head;
+  int tail;
+  Event contents[MAX_NUMBER_EVENTS];
+} EventQueue;
 
-
+EventQueue * enqueue(EventQueue *eq, Event e) {
+  if(eq->tail + 1 == eq->head
+  ||(eq->tail + 1 >= MAX_NUMBER_EVENTS && eq->head == 0)) {
+    // we're full, so print that and drop the event
+    printf("Event queue is full. (Head: %d, tail: %d)", eq->head, eq->tail);
+  }
+  else {
+    int newTail = eq->tail + 1;
+    if(newTail >= MAX_NUMBER_EVENTS) newTail = 0;
+    eq->contents[newTail] = e;
+    eq->tail = newTail;
+  }
+  return eq;
+}
 
 
 /*****************************************************************************\
 *                                  Global data                                *
 \*****************************************************************************/
-
-
+EventQueue queue;
 
 
 /*****************************************************************************\
@@ -89,8 +106,19 @@ void Control(void){
 \***********************************************************************/
 void InterruptRoutineHandlerDevice(void){
   printf("An event occured at %f  Flags = %d \n", Now(), Flags);
-  DisplayEvent('m', &BufferLastEvent[Flags]);
   // Put Here the most urgent steps that cannot wait
+  // Determine device id
+  Identifier interruptingDevice = 0;
+  int x;
+  for(x = 0; x < 32; x++) {
+    if(Flags >= exp2(x)) {
+      interruptingDevice = x;
+    }
+    else break;
+  }
+  DisplayEvent('C', &BufferLastEvent[interruptingDevice]);
+  queue = *enqueue(&queue, BufferLastEvent[interruptingDevice]);
+  Flags -= exp2(interruptingDevice);
 }
 
 
